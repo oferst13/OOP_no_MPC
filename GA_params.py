e1b529ef27ad438df1b5db5fa7500768925508e1
oferst13 <66622305+oferst13@users.noreply.github.com>
2022-08-02 12:47:51 +0300

    finished GA

--- GA_params.py
+++ GA_params.py
@@ -5,8 +5,8 @@ num_generations = 250
 gene_space = [0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 parent_selection = "rank"
 crossover_type = "uniform"
-crossover_prob = 0.3
-mutation_prob = 0.05
+crossover_prob = 0.4
+mutation_prob = 0.07
 mutation_by_replacement = True
 mutation_type = "random"
 stop_criteria = "saturate_80"
@@ -14,7 +14,7 @@ stop_criteria = "saturate_80"
 
 def pop_init(release_hr, n_tanks):
     num_genes = release_hr * n_tanks
-    num_sols = math.ceil(num_genes * 1.5)
+    num_sols = math.ceil(num_genes * 1.3)
     sol_zero = np.zeros(num_genes)
     sol_one = np.ones(num_genes) * gene_space[0]
     sol_two = np.ones(num_genes) * gene_space[1]
